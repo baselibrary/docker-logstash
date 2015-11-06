79b9099c71c9e682500e250d6d19e8f3073cc7bb
Shawn Ma <qsma@thoughtworks.com>
2015-11-06 09:27:59 +0800

    update

--- update.sh
+++ update.sh
@@ -10,14 +10,14 @@ fi
 versions=( "${versions[@]%/}" )
 
 
-for version in "${versions[@]}"; do	
-  fullVersion="$(curl -fsSL "http://packages.elastic.co/logstash/$version/debian/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk awk -v pkgname="logstash" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+for version in "${versions[@]}"; do
+	fullVersion="$(curl -fsSL "http://packages.elastic.co/logstash/$version/debian/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname="logstash" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
 	(
 		set -x
 		cp docker-entrypoint.sh "$version/"
 		sed '
-			s/%%MAJOR%%/'"$version"'/g;
-			s/%%VERSION%%/'"$fullVersion"'/g;
+			s/%%LOGSTASH_MAJOR%%/'"$version"'/g;
+			s/%%LOGSTASH_VERSION%%/'"$fullVersion"'/g;
 		' Dockerfile.template > "$version/Dockerfile"
 	)
 done
