d7fab9425c73b0e3d035433358eb0ffce0d87470
Shawn Ma <qsma@thoughtworks.com>
2016-03-18 23:48:15 +0800

    更新编译脚本

--- update.sh
+++ update.sh
@@ -11,7 +11,8 @@ versions=( "${versions[@]%/}" )
 
 
 for version in "${versions[@]}"; do
-	fullVersion="$(curl -fsSL "http://packages.elastic.co/logstash/$version/debian/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname="logstash" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+	repoPackage="http://packages.elastic.co/logstash/$version/debian/dists/stable/main/binary-amd64/Packages.gz"
+  fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="logstash" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
 	(
 		set -x
 		cp docker-entrypoint.sh "$version/"
